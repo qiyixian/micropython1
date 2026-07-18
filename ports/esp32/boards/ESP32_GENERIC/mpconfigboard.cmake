0a714a7b377a8a29e36a209638838d3209cf9bd0
qiyixian <zhoupenghang@126.com>
2026-07-18 18:01:02 +0800

    Update mpconfigboard.cmake
    Signed-off-by: qiyixian <zhoupenghang@126.com>

--- ports/esp32/boards/ESP32_GENERIC/mpconfigboard.cmake
+++ ports/esp32/boards/ESP32_GENERIC/mpconfigboard.cmake
@@ -2,3 +2,5 @@ include(boards/mpconfigboard_esp32_common.cmake)
 
 list(APPEND SDKCONFIG_DEFAULTS
     boards/sdkconfig.csi)
+
+set(SDKCONFIG_OPTS "${SDKCONFIG_OPTS} -DCONFIG_TWAI=y -DCONFIG_TWAI_ISR_IN_IRAM=y")
